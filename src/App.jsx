4bff28bfbdcca258bf5ae69fb5a2b94909407fcb
“muneeb <muneebfarhan>
2025-02-21 20:52:53 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-400'>hello</h1>
+      <p className='text-blue-500'>uhytr</p>
+    </>
+  )
+}
+
+export default App
